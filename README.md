a6d4f67edaddeec7a03c07585582d68348b495f2
Aurélien GIRY <aurelien.giry@gmail.com>
2019-01-13 20:02:09 +0100

    TypeScript: hello-world && leap

---
+++ README.md
@@ -0,0 +1,7 @@
+# Excercism TypeScript
+
+TypeScript exercises from [Exercism.io](https://exercism.io)
+
+- Hello World
+- Leap
+
---
+++ leap/leap.ts
@@ -0,0 +1,16 @@
+function isLeapYear( year: number ) {
+    // Your code here
+    if (year % 400 === 0) {
+        return true
+    }
+    else if (year % 100 === 0) {
+        return false
+    }
+    else if (year % 4 === 0) {
+        return true
+    }
+
+    return false
+}
+
+export default isLeapYear
