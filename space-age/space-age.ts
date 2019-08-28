021a2ebc77b785dce9ff76574967a23e6f2901b5
Aurélien GIRY <aurelien.giry@gmail.com>
2019-08-28 23:01:14 +0200

    Typescript: [SpaceAge] init class SpaceAge

---
+++ space-age/space-age.ts
@@ -0,0 +1,41 @@
+class SpaceAge
+{
+    seconds: number;
+    
+    constructor(seconds: number) {
+        this.seconds = seconds;
+    }
+    onEarth() {
+
+    }
+
+    onMars() {
+        
+    }
+
+    onMercury() {
+        
+    }
+
+    onVenus() {
+        
+    }
+
+    onJupiter() {
+        
+    }
+
+    onSaturn() {
+
+    }
+
+    onUranus() {
+
+    }
+
+    onNeptune() {
+
+    }
+}
+
+export default SpaceAge
