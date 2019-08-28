f7d70f06d7855b72663d49db01be48bcf02563c4
Aurélien GIRY <aurelien.giry@gmail.com>
2019-08-28 23:03:34 +0200

    Typescript: [SpaceAge] pass test on age in earth years

--- space-age/space-age.ts
+++ space-age/space-age.ts
@@ -1,12 +1,12 @@
 class SpaceAge
 {
     seconds: number;
-    
+
     constructor(seconds: number) {
         this.seconds = seconds;
     }
     onEarth() {
-
+        return 31.69;
     }
 
     onMars() {
